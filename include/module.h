86e2f3cc096dae5e73d9b9a1aa914aa830b5b188
Mikhail Konyukhov (aka piroman-lynx) <piroman@myconfig.ru>
2014-08-07 05:22:44 +0400

    [+] explorer.c

--- include/module.h
+++ include/module.h
@@ -4,36 +4,25 @@
 struct call_description {
   const char *call;
   unsigned char args;
-  const char *arg_0;
-  const char *arg_1;
-  const char *arg_2;
-  const char *arg_3;
-  const char *arg_4;
-  const char *arg_5;
-  const char *arg_6;
-  const char *arg_7;
-  const char *arg_8;
-  const char *arg_9;
-  const char *arg_10;
-  const char *arg_11;
-  const char *arg_12;
-  const char *arg_13;
-  const char *arg_14;
-  const char *arg_15;
-  void* function;
+  const char *args_array[20];
+  const char *function;
 };
 
 struct module_description {
-  unsigned char may_init;
+/*  unsigned char may_init;
   unsigned char must_init;
   unsigned char may_finish;
-  unsigned char must_finish;
-  void* exports_argc;
-  void* exports_argv;
+  unsigned char must_finish;*/
+  unsigned int(*exports_argc)();
+  struct call_description(*exports_argv)(unsigned int);
 };
 
 //void is_cc_module_explore(struct module_description);
 
-#define MODULE_REGISTER(x) static const struct module_description *module_for_explore = &x;
+//static const struct module_description *is_cc_module_for_explore;
+
+#define MODULE_REGISTER(x) is_cc_register(x);
+
+#define TRANSLATION_ERROR(moduleName, message) is_cc_call_translation_error(moduleName, message);
 
 #endif
